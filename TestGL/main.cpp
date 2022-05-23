abd837de0789f792ae65079df718ea5448acecf1
Alvi Hasan <livealvi@gmail.com>
2022-05-23 15:32:43 +0600

    Initial Commit

---
+++ TestGL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TestGL
+//
+//  Created by Alvi Hasan on 23/5/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
